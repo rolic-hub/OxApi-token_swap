e6e4c43fc693c0a5795919a14ac6ea7197169be0
rolic-hub <enyiomaeze09@gmail.com>
2022-08-17 22:01:34 +0100

    update repo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Main from "./components/main"
 
+
 function App() {
   return (
     <div >
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,80 +1,103 @@
-import React from 'react'
-import {MdSwapVerticalCircle} from "react-icons/md"
-import {ethers} from "ethers"
-import {useState, useEffect} from "react"
+import React from "react";
+import { MdSwapVerticalCircle } from "react-icons/md";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
+import { Alchemy } from "alchemy-sdk";
 
 const Main = () => {
-    const [account, setAccount] = useState(null)
-    const [connected, setConnected] = useState(false)
-    const [tokenList, setTokenList] = useState([])
-    const {ethereum} = window;
-    const connectWallet = async() => {
-         if(typeof ethereum == "undefined") return alert("please install metamask")
-       try {
-        console.log("connecting to metamask")
-        await  ethereum.request({ method:  "eth_requestAccounts" });
-         const accounts = await ethereum.request({ method: "eth_accounts" });
-         //console.log(accounts[0])
-         setAccount(accounts[0])
-         setConnected(true)
-       } catch (error) {
-        console.log(error)
-       }
-    }
-     const getListOfTokens = async () => {
-        const listResult = await fetch("https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json")
-        const response = await listResult.json()
-        const data = response.tokens;
-        setTokenList(data)
+  const [account, setAccount] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [tokenList, setTokenList] = useState([]);
+  const { ethereum } = window;
 
-        console.log(data)
-     }
-     useEffect(() => {
-     getListOfTokens()
-     }, [])
+  const connectWallet = async () => {
+    if (typeof ethereum == "undefined") return alert("please install metamask");
+    try {
+      console.log("connecting to metamask");
+      await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      //console.log(accounts[0])
+      setAccount(accounts[0]);
+      setConnected(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // const options = tokenList.map((token) => {
+  // token.symbol
+  // })
+  const getListOfTokens = async () => {
+    const listResult = await fetch(
+      "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json"
+    );
+    const response = await listResult.json();
+    const data = response.tokens;
+    setTokenList(data);
+
+    // console.log(data);
+  };
+  useEffect(() => {
+    getListOfTokens();
+  }, []);
 
   return (
-    <div >
-        <button className="border-4 border-indigo-200 text-xl mt-24 p-2" onClick={connectWallet}>{ connected ? 
-        <p>{account.slice(0,5)}...{account.slice(37,42)}</p> :
-        <p> connect wallet</p>}</button>
-    <div className="mt-16 border rounded-lg bg-black text-white justify-center w-fit flex p-8 flex-col">
-      <p>
-        Swap
-      </p>
-      
-      <div className="bg-slate-600 w-fit mb-2 pb-8 p-2 rounded-md" >
-      <label for="from-tokens">you pay</label>
-      <div className="flex p-3 flex-row">
-      <select selected="default" name="tokens-to-swap" id="from-tokens">
-        <option value="default" selected="default">select a token</option>
-            {tokenList.map((token) => (
-           <option value={token.symbol} size={10}>{token.symbol}</option>))}
-        </select>
-        <input className="ml-4" type="number"/>
-      </div>
-      </div>
-      <div  className="absolute mb-20" > 
-              <MdSwapVerticalCircle size="50px"/>
-      </div>
+    <div>
+      <button
+        className="border-4 border-indigo-200 text-xl mt-24 p-2"
+        onClick={connectWallet}
+      >
+        {connected ? (
+          <p>
+            {account.slice(0, 5)}...{account.slice(37, 42)}
+          </p>
+        ) : (
+          <p> connect wallet</p>
+        )}
+      </button>
 
-      <div className="bg-slate-600 pt-8 w-fit p-2 rounded-lg" >
-         <label for="to-tokens">you recieve</label>
-     <div className="flex p-3  flex-row">
-      <select name="tokens to swap" id="to-tokens">
-            <option>tokens</option>
-            <option>polygon</option>
-        </select>
-        <input className="ml-4" type="number"/>
-     </div>
-     
-      </div>
-      <p className="mt-2"> Estimated Gas:</p>
-      <button className="bg-sky-700 mt-8 ml-20 w-fit p-2 rounded-md">swap token </button>
-    </div>
-    </div>
-   
-  )
-}
+      <div className="mt-16 border rounded-lg bg-black text-white justify-center w-fit flex p-8 flex-col">
+        <p>Swap</p>
 
-export default Main
+        <div className="bg-slate-600 w-fit mb-2 pb-8 p-2 rounded-md">
+          <label for="from-tokens">you pay</label>
+          <div className="flex p-3 flex-row">
+            <div className="flex flex-col">
+              <p>select token </p>
+              <ul className="absolute mt-16">
+                <li>eeee</li>
+                <li>eeee</li>
+                <li>eeee</li>
+                <li>eeee</li>
+              </ul>
+            </div>
+            <div>
+               <input className="ml-7" type="number" />
+            </div>
+           
+           
+          </div>
+        </div>
+        <div className="absolute mb-20">
+          <MdSwapVerticalCircle size="50px" />
+        </div>
+
+        <div className="bg-slate-600 pt-8 w-fit p-2 rounded-lg">
+          <label for="to-tokens">you recieve</label>
+          <div className="flex p-3  flex-row">
+            <select name="tokens to swap" id="to-tokens">
+              <option>tokens</option>
+              <option>polygon</option>
+            </select>
+            <input className="ml-4" type="number" />
+          </div>
+        </div>
+        <p className="mt-2"> Estimated Gas:</p>
+        <button className="bg-sky-700 mt-8 ml-20 w-fit p-2 rounded-md">
+          swap token{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
